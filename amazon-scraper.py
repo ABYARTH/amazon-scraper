49d4c6b7a067f9a2685051f55d88ef9fed44d160
Abyarth Kumar Behera <abyarthkumarb5@gmail.com>
2017-06-23 01:15:12 +0530

    Add files via upload

--- amazon-scraper.py
+++ amazon-scraper.py
@@ -1,67 +1,60 @@
-#import socks
-#import socket
-#
-#import stem.process
-#
-#SOCKS_PORT=7000# You can change the port number
-#
-#tor_process = stem.process.launch_tor_with_config(
-#    config = {
-#        'SocksPort': str(SOCKS_PORT),
-#    },
-#)
-#
-#
-#socks.setdefaultproxy(proxy_type=socks.PROXY_TYPE_SOCKS5, addr="127.0.0.1", port=SOCKS_PORT)
-#
-#socket.socket = socks.socksocket
-
 from bs4 import BeautifulSoup
 import urllib
+from time import sleep
+import csv
 
-arr = []
-keyword = "samsung"
-for i in range(1,10):
-    i = str(i)
-    url = "http://www.amazon.in/s/ref=sr_pg_"+i+"?rh=n%3A976419031%2Ck%3A"+keyword+"&page="+i+"&keywords="+keyword
+import sys
+reload(sys)
+sys.setdefaultencoding("utf8")
 
-    r = urllib.urlopen(url).read()
-    soup = BeautifulSoup(r)
+def scrape():
+	arr = []
+	keyword = "samsung"
+	for i in range(1,30):
+	    i = str(i)
+	    url = "http://www.amazon.in/s/ref=sr_pg_"+i+"?rh=n%3A976419031%2Ck%3A"+keyword+"&page="+i+"&keywords="+keyword
 
-    content = soup.find_all("a", class_="a-link-normal s-access-detail-page  s-color-twister-title-link a-text-normal")
+	    r = urllib.urlopen(url).read()
+	    soup = BeautifulSoup(r)
 
-#    print content
+	    content = soup.find_all("a", class_="a-link-normal s-access-detail-page  s-color-twister-title-link a-text-normal")
 
-    for c in content:
-        try:
-            label = c.get('href')
-            sublabel = label.split("/")
+	    for c in content:
+		try:
+		    label = c.get('href')
+		    sublabel = label.split("/")
+		    arr.append(sublabel[5])
+		except:
+		    pass
 
-            print sublabel
+	title_arr = []
 
-            arr.append(sublabel[5])
+	for i in range(0, len(arr)):
+	    try:
+			sleep(0.5)
+			uri = "http://www.amazon.in/dp/"+arr[i]+"/"
+			dt = urllib.urlopen(uri).read()
+			cont = BeautifulSoup(dt)
 
-        except:
-            pass
+			content = cont.find_all("span", class_="a-size-large")
+			for conte in content:
+			    p = conte.text.strip()
+			    if (p != '+'):
+			    	title_arr.append(p)
+			    if len(title_arr) == 10:
+			    	break
+	    except:
+			pass
 
-#print arr
+	print title_arr
 
-title_arr = []
+	datafile = open('datafile.csv', 'wb')
+	fieldnames = ['products']
+	writer = csv.DictWriter(datafile, fieldnames=fieldnames)
+	writer.writeheader()
+	for r in title_arr:
+		writer.writerow({'products': r.encode("utf-8")})
+	datafile.close()
 
-for i in range(0, len(arr)):
-    try:
-        uri = "http://www.amazon.in/dp/"+arr[i]+"/"
-        dt = urllib.urlopen(uri).read()
-        cont = BeautifulSoup(dt)
 
-        content = cont.find_all("span", class_="a-size-large")
-        for conte in content:
-            p = conte.text.strip()
-#            print p
-            title_arr.append(p)
-    except:
-        pass
-
-print title_arr    
-    
-#tor_process.kill()
+scrape()
